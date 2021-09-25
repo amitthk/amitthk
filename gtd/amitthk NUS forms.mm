f2f994bda44c9acc60e79de9cbd38c2395b2a440
amitthk <e.amitthakur@gmail.com>
2021-09-25 14:17:02 +0800

    projects

---
+++ gtd/amitthk NUS forms.mm
@@ -0,0 +1,32 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1632029756218" ID="ID_479499139" MODIFIED="1632032497560" TEXT="amitthk-personal-profile">
+<node CREATED="1632029766399" ID="ID_1275751162" MODIFIED="1632030720361" POSITION="right" TEXT="Personal letter">
+<node CREATED="1632030585760" ID="ID_681501020" MODIFIED="1632030591173" TEXT="Mid and long term goals"/>
+<node CREATED="1632030592369" ID="ID_1952356553" MODIFIED="1632030595932" TEXT="Experience to date"/>
+<node CREATED="1632030596202" ID="ID_73149034" MODIFIED="1632030600893" TEXT="The NUS MBA"/>
+<node CREATED="1632030601220" ID="ID_1743801459" MODIFIED="1632030605893" TEXT="In class experience"/>
+<node CREATED="1632030607127" ID="ID_468226484" MODIFIED="1632030613437" TEXT="Experiential Learning"/>
+<node CREATED="1632030614112" ID="ID_223793873" MODIFIED="1632030620469" TEXT="Out of class experience"/>
+<node CREATED="1632030620727" ID="ID_1639507034" MODIFIED="1632030624485" TEXT="The Network"/>
+</node>
+<node CREATED="1632030721577" ID="ID_131556434" MODIFIED="1632030735890" POSITION="right" TEXT="People, events, situations influenced">
+<node CREATED="1632030736773" ID="ID_1931368288" MODIFIED="1632032069184" TEXT="started in family with farmer background , not much awareness around avenues after education"/>
+<node CREATED="1632032069884" ID="ID_279289607" MODIFIED="1632032095878" TEXT="father focussed on providing good education to us siblings"/>
+<node CREATED="1632030762949" ID="ID_1299676791" MODIFIED="1632032132558" TEXT="worked for government forest department created system for importing their employee database and website"/>
+<node CREATED="1632032133921" ID="ID_1095698641" MODIFIED="1632032188502" TEXT="reached Bangalore and worked as freelancer then joined E&amp;Y. Recognized for migrating their long outstandding apps to consolicated framework. Front-end and backend."/>
+<node CREATED="1632030793187" ID="ID_967794225" MODIFIED="1632032228830" TEXT="Got opportunity to work at NUS FASS. Developed vvarious websites and web-apps. weather statinon. migrated apps"/>
+<node CREATED="1632030804508" ID="ID_1170232242" MODIFIED="1632032238231" TEXT="managed multi country projects at MIMS"/>
+<node CREATED="1632030823768" ID="ID_563015341" MODIFIED="1632032250015" TEXT="did a full DevOps toolchain assessment and implemented DevOps at Standard Chartered under Accenture"/>
+<node CREATED="1632030840782" ID="ID_1161068837" MODIFIED="1632032260801" TEXT="Built Capacity governance framework for Big Data Platform from Scratch">
+<node CREATED="1632032265049" ID="ID_1786772126" MODIFIED="1632032278777" TEXT="Big Data is a new nieche technology"/>
+<node CREATED="1632032279090" ID="ID_1437319914" MODIFIED="1632032299468" TEXT="I have gained experience in data analytics toolchain"/>
+<node CREATED="1632032299765" ID="ID_1619352652" MODIFIED="1632032306041" TEXT="built data driven solutions"/>
+</node>
+<node CREATED="1632032309416" ID="ID_159980067" MODIFIED="1632032338913" TEXT="Want to take my experience to provide bigger scale of value to organizations"/>
+<node CREATED="1632032364284" ID="ID_1309097187" MODIFIED="1632032445037" TEXT="Through my experience I have learnt that building frameworks which allow people to utilise new technologies - builds opportunities for everyone including us to bring value to socieety as a whole."/>
+<node CREATED="1632038658288" ID="ID_1925195132" MODIFIED="1632038783655" TEXT="Through my personal life experience I have learnt that technology and education are greate enablers. We can use both to build foundation for everyone to uplift themseleves  and others"/>
+<node CREATED="1632038718677" ID="ID_1264418946" MODIFIED="1632038749370" TEXT="I want to use what I have learnt over the period of years to enable future generations "/>
+</node>
+</node>
+</map>
